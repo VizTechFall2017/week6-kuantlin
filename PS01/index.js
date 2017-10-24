96f96aaec55c3ae41285f635b16a2fe50637596f
kuantlin <31868081+kuantlin@users.noreply.github.com>
2017-10-24 00:19:18 -0400

    Assignment 6

--- PS01/index.js
+++ PS01/index.js
@@ -10,7 +10,7 @@ var svg = d3.select('svg')
     .append('g')
     .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
-var scaleX = d3.scaleBand().rangeRound([0, 600]).padding(0.1);
+var scaleX = d3.scaleBand().rangeRound([0, 1000]).padding(0.1);
 var scaleY = d3.scaleLinear().range([400, 0]);
 
 
@@ -21,16 +21,16 @@ d3.csv('./Clean Energy.csv', function(dataIn){
         .key(function(d){return d.year})
         .entries(dataIn);
 
-    var loadData = nestedData.filter(function(d){return d.key == '2010'})[0].values;
+    var loadData = nestedData.filter(function(d){return d.key == '2006'})[0].values;
 
     scaleX.domain(loadData.map(function(d){return d.name;}));
-    scaleY.domain([0, d3.max(loadData.map(function(d){return +d.total}))]);
+    scaleY.domain([0, d3.max(loadData.map(function(d){return +d.clean}))]);
 
 
 
     svg.append('text')
-        .text('Hawaii Clean Energy Breakdown')
-        .attr('transform','translate(300, -50)')
+        .text('Clean Energy Produced by States')
+        .attr('transform','translate(450, -50)')
         .style('text-anchor','middle')
         .style("font-size",'28px')
     ;
@@ -39,7 +39,18 @@ d3.csv('./Clean Energy.csv', function(dataIn){
 
     svg.append("g")
         .attr('transform','translate(0,400)')
-        .call(d3.axisBottom(scaleX));
+        .call(d3.axisBottom(scaleX))
+        .selectAll("text")
+        .attr("dx", "-.8em")
+        .attr("dy", ".25em")
+        .attr("transform", "rotate(-60)" )
+        .style("text-anchor", "end")
+        .attr("font-size", "10px");;
+
+    svg.append('text')
+        .text('Generated Clean Energy, in MWh')
+        .attr('transform', 'translate(-30,345)rotate(270)');
+
 
     svg.append("g")
         .attr('class','yaxis')
@@ -51,13 +62,10 @@ d3.csv('./Clean Energy.csv', function(dataIn){
         .enter()
         .append('rect')
         .attr('class','bars')
-        .style("fill", function(d,i) { return 'rgb(20, ' + ((i * 30) + 50) + ', 20)'});
+        .style("fill", function(d,i) { return 'rgb(20, ' + ((i * 5) + 50) + ', 20)'});
 
 
 
-
-    $('#testRect').tooltip();
-    ;
 
 
     drawPoints(loadData);
@@ -68,7 +76,7 @@ d3.csv('./Clean Energy.csv', function(dataIn){
 
 function drawPoints(pointData){
 
-    scaleY.domain([0, d3.max(pointData.map(function(d){return +d.total}))]); //change the domain along with the years with data, rescale the data!
+    scaleY.domain([0, d3.max(pointData.map(function(d){return +d.clean}))]); //change the domain along with the years with data, rescale the data!
 
     svg.selectAll('.yaxis')
         .call(d3.axisLeft(scaleY));
@@ -79,17 +87,17 @@ function drawPoints(pointData){
             return scaleX(d.name);
         })
         .attr('y',function(d){
-            return scaleY(d.total);
+            return scaleY(d.clean);
         })
         .attr('width',function(d){
             return scaleX.bandwidth();
         })
         .attr('height',function(d){
-            return 400 - scaleY(d.total);  //400 is the beginning domain value of the y axis, set above range[400,0]
+            return 400 - scaleY(d.clean);  //400 is the beginning domain value of the y axis, set above range[400,0]
         })
-        .attr('data-toggle', 'tooltip');
+
 }
-    $('[data-toggle="tooltip"]').tooltip();
+
 
 function updateData(selectedYear){
     return nestedData.filter(function(d){return d.key == selectedYear})[0].values;
